0227e2d6eeb2cc23bdb2cd4e800a285c26f903d0
wsz82 <wojteksz82@gmail.com>
2023-03-04 20:08:01 +0100

    Divide modules

--- api/src/main/java/spio2023/calibrationmanagementsoftware/api/configuration/LoadDatabase.java
+++ api/src/main/java/spio2023/calibrationmanagementsoftware/api/configuration/LoadDatabase.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sample.SampleData_BC06;
+import sample.SampleData_PP_METEX_3610;
 import spio2023.calibrationmanagementsoftware.api.database.device.DeviceRepository;
 import spio2023.calibrationmanagementsoftware.api.database.device.TestDevice;
 import spio2023.calibrationmanagementsoftware.api.database.instrument.InstrumentRepository;
@@ -22,8 +23,8 @@ public class LoadDatabase {
         return args -> {
             log.info("Preloading " + instrumentRepository.save(new ReferenceInstrument(SampleData_BC06.thermometer_P755())));
             log.info("Preloading " + deviceRepository.save(new TestDevice(SampleData_BC06.thermometer_BC06(), new ProcedureData("BC06", SampleData_BC06.procedure()))));
-//            log.info("Preloading " + instrumentRepository.save(new ReferenceInstrument(SampleData_PP_METEX_3610.multimeter_INMEL7000())));
-//            log.info("Preloading " + deviceRepository.save(new TestDevice(SampleData_PP_METEX_3610.multimeter_PP_METEX_3610(), new ProcedureData("PP METEX3610", SampleData_PP_METEX_3610.procedure()))));
+            log.info("Preloading " + instrumentRepository.save(new ReferenceInstrument(SampleData_PP_METEX_3610.multimeter_INMEL7000())));
+            log.info("Preloading " + deviceRepository.save(new TestDevice(SampleData_PP_METEX_3610.multimeter_PP_METEX_3610(), new ProcedureData("PP METEX3610", SampleData_PP_METEX_3610.procedure()))));
         };
     }
 
